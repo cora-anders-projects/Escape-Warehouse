bf43a47a5a23e1fe7ab1a9ec36aa65c3b032941b
Rubini 07 <user@FIH-COM23a107.fsu.edu>
2026-03-02 16:31:29 -0500

    more shelves, conyer made, script to move object right made

---
+++ Assets/conveyormove.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class conveyormove : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+    }
+}
