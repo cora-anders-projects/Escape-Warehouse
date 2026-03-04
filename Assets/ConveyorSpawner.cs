f09c0a8023bccce080fe1431613f17898b97348b
Rubini 07 <user@FIH-COM23a107.fsu.edu>
2026-03-04 17:23:56 -0500

    conveyor script works, map finished, materials added

---
+++ Assets/ConveyorSpawner.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ConveyorSpawner : MonoBehaviour
+{
+    public GameObject Conveyor;
+    public float spawnRate = 1.5f;
+    private float timer = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        } else
+        {
+            spawnPipe();
+            timer = 0;
+        }
+    }
+   void spawnPipe()
+    {
+        Instantiate(Conveyor, transform.position, transform.rotation);
+    }
+}
--- Assets/conveyormove.cs
+++ Assets/conveyormove.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class conveyormove : MonoBehaviour
 {
-    public float moveSpeed = 1;
+    public float moveSpeed = 0.0001f;
+    public float deadZone = -17;
+
+ 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,5 +17,7 @@ public class conveyormove : MonoBehaviour
     void Update()
     {
         transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        if (transform.position.x < deadZone)
+        { Destroy(gameObject); }
     }
 }
